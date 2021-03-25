0f1ffbdff8a8e97ae4239260a47a80079608c2ca
Francesco Paciello <kekkopacio@gmail.com>
2021-03-25 17:56:27 +0100

    Initial Commit

---
+++ Animating Circles/Animating_CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_CirclesApp.swift
+//  Animating Circles
+//
+//  Created by Kekko Paciello on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Animating_CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
